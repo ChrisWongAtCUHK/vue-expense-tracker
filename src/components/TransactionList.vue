efa02f67ca8ff157ad7279abf902a4f837e9105d
Chris Wong <chris.wong.ksl@gmail.com>
2026-06-12 11:38:35 +0800

    Transactions as props

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+import { defineProps } from 'vue'
 
-const transactions = ref([
-  { id: 1, text: 'Flower', amount: -19.99 },
-  { id: 2, text: 'Salary', amount: 299.97 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 },
-])
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+})
 </script>
 <template>
   <h3>History</h3>
